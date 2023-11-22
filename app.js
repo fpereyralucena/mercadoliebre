5f929147e936f0441635e9df4204ec620076b469
Francisco Pereyra Lucena <fpereyralucena@gmail.com>
2023-11-21 21:04:05 -0300

    arreglo envio de formulario y transiciones

--- app.js
+++ app.js
@@ -11,8 +11,17 @@ app.get('/', (req, res) => {
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'))
 })
+
+app.post("/register.html", (req, res) => {
+    res.send('Recibido el formulario de registro');
+})
+
+
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, './views/login.html'))
+})
+app.post("/login.html", (req, res) => {
+    res.send('Recibido el formulario de login');
 })
 
 app.get("*", (req, res) => {
