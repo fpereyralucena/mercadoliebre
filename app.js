94a4464c136a60c392610a5a6fc76aee7132a032
Francisco Pereyra Lucena <fpereyralucena@gmail.com>
2023-11-23 21:42:36 -0300

    arreglo port

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT_LISTENING = 3000
+const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
-app.listen(PORT_LISTENING, ()=>console.log("server runnning http://127.0.0.1:"+PORT_LISTENING));
+app.listen(PORT, ()=>console.log("server runnning http://127.0.0.1:${PORT}"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
