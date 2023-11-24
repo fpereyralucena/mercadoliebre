79a266b1e19b070ce01433c84ff1324c7fb70459
Francisco Pereyra Lucena <fpereyralucena@gmail.com>
2023-11-24 17:03:59 -0300

    arreglo buscador y login

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
-app.listen(PORT, ()=>console.log("server runnning http://127.0.0.1:{$PORT}"));
+app.listen(PORT, ()=>console.log(`server runnning http://127.0.0.1:${PORT}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
